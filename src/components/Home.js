f65228f32df63dd5508993e32504b56c7005454e
DOROTHY2322 <kimelidorothy@gmail.com>
2023-03-04 11:34:27 +0300

    new components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div className="home-container" style={{backgroundImage: "url('/path/to/background-image.jpg')"}}>
+    <div className="home-container" style={{backgroundImage: "url(https://github.com/DOROTHY2322/phase--3-petfinder-client/blob/main/public/background.jpg)"}}>
       ...
       <div className="feature">
         <img src="/path/to/cat-image.jpg" alt="Cat" />
@@ -11,7 +11,7 @@ function Home() {
         <p>Support animal welfare and help us care for our animals by making a donation.</p>
       </div>
       <div className="feature">
-        <img src="/path/to/bird-image.jpg" alt="Bird" />
+        <img src="https://media.istockphoto.com/id/480202588/photo/parrot-puppy-and-cat.jpg" alt="Bird" />
         <h2>Support Our Mission</h2>
         <p>Support animal welfare and help us care for our animals by making a donation.</p>
       </div>
