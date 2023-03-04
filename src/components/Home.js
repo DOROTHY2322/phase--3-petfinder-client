38dbed3289749a793a47601b8c6f52b79a01e6bf
DOROTHY2322 <kimelidorothy@gmail.com>
2023-03-04 11:28:01 +0300

    new components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,31 +3,17 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div className="home-container">
-      <div className="home-banner">
-        <h1>Welcome to Pet Finder!</h1>
-        <p>Find your perfect furry friend today.</p>
-        <Link to="/pets" className="btn btn-primary">
-          See Our Pets
-        </Link>
+    <div className="home-container" style={{backgroundImage: "url('/path/to/background-image.jpg')"}}>
+      ...
+      <div className="feature">
+        <img src="/path/to/cat-image.jpg" alt="Cat" />
+        <h2>Support Our Mission</h2>
+        <p>Support animal welfare and help us care for our animals by making a donation.</p>
       </div>
-      <div className="home-features">
-        <div className="feature">
-        {/* <a href="https://example.com/image.jpg">View image</a> */}
-
-          <h2>Find Your Perfect Pet</h2>
-          <p>Browse our selection of adoptable pets and find your perfect match.</p>
-        </div>
-        <div className="feature">
-          {/* <img src={catImage} alt="Cat" /> */}
-          <h2>Adopt a Pet</h2>
-          <p>Give a pet a forever home and make a lifelong companion.</p>
-        </div>
-        <div className="feature">
-          {/* <img src={birdImage} alt="Bird" /> */}
-          <h2>Support Our Mission</h2>
-          <p>Support animal welfare and help us care for our animals by making a donation.</p>
-        </div>
+      <div className="feature">
+        <img src="/path/to/bird-image.jpg" alt="Bird" />
+        <h2>Support Our Mission</h2>
+        <p>Support animal welfare and help us care for our animals by making a donation.</p>
       </div>
     </div>
   );
